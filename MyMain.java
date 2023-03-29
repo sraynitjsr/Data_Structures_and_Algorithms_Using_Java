22aeb20cb5ce57d09488febffd3233e96c034ef0
Subhradeep Ray <subhradeepray2017@gmail.com>
2023-03-29 23:10:20 +0530

    ArrayList

--- MyMain.java
+++
@@ -1,5 +0,0 @@
-public class MyMain {
-	public static void main(String[] args) throws Exception {
-		System.out.print("Welcome to the world of Java........");
-	}
-}
---
+++ com/sraynitjsr/main/MyMain.java
@@ -0,0 +1,11 @@
+package com.sraynitjsr.main;
+
+import com.sraynitjsr.dsa.*;
+
+public class MyMain {
+	public static void main(String[] args) throws Exception {
+		System.out.print("Welcome to the world of Java\n");
+		MyInputOutput.start();
+		MyArrayList.start();
+	}
+}
