09cd8cde2b98c5dc7c873987aa75000b3be7e5b6
Subhradeep Ray <subhradeepray2017@gmail.com>
2023-03-31 18:23:39 +0530

    DSA Using Java

---
+++ MyMain.java
@@ -0,0 +1,7 @@
+public class MyMain {
+	public static void main(String[] args) throws Exception {
+		System.out.print("\n....Welcome to the world of Java....");
+		
+		System.out.println("\n");
+	}
+}
