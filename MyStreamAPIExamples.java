3cc63195ef1ec530f5cf47c9c7ed9b899474c54a
Subhradeep Ray <subhradeepray2017@gmail.com>
2023-06-11 01:26:40 +0530

    Case One, Empty String Count

--- MyStreamAPIExamples.java
+++ MyStreamAPIExamples.java
@@ -1,5 +1,10 @@
+import java.util.*;
+
 public class MyStreamAPIExamples {
     public static void start() {
-        System.out.println("Inside Stream API Usage");
+        System.out.println("\nInside Stream API Usage");
+        List<String> strList = Arrays.asList("abc", "", "bcd", "", "defg", "jk");
+        long count = strList.stream().filter(x -> x.isEmpty()).count();
+        System.out.println("Case One, Empty String Count is => " + strList + " " + count);
     }
 }
