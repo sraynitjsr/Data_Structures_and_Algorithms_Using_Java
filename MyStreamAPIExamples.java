b4f5cf6c74e81309caa90ae0ae67ac2be79fefe7
Subhradeep Ray <subhradeepray2017@gmail.com>
2023-06-11 22:09:36 +0530

    Case two, Count String whose length is more than three

--- MyStreamAPIExamples.java
+++ MyStreamAPIExamples.java
@@ -9,7 +9,7 @@ public class MyStreamAPIExamples {
         long count = strList.stream().filter(x -> x.isEmpty()).count();
         System.out.println("Case One, Empty String Count is => " + strList + " " + count);
 
-        long num = strList.stream() .filter(x -> x.length()> 3) .count();
+        long num = strList.stream().filter(x -> x.length()> 3).count();
         System.out.println("Case Two, Count Strings Whose Length is More Than Three" + num);
     }
 }
