d0e7812b6e444c6d5e0c4a741583a0445ac85ad1
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2024-01-22 23:39:09 +0530

    Adjacency List and Adjacency Matrix Representations

--- MyGraph.java
+++ MyGraph.java
@@ -2,9 +2,11 @@ import java.util.*;
 
 class Graph {
     private Map<Integer, List<Integer>> adjacencyList;
+    private int[][] adjacencyMatrix;
 
-    public Graph() {
+    public Graph(int numVertices) {
         this.adjacencyList = new HashMap<>();
+        this.adjacencyMatrix = new int[numVertices][numVertices];
     }
 
     public void addVertex(int vertex) {
@@ -13,13 +15,26 @@ class Graph {
 
     public void addEdge(int source, int destination) {
         this.adjacencyList.get(source).add(destination);
+        this.adjacencyList.get(destination).add(source); // for undirected graph
+
+        this.adjacencyMatrix[source][destination] = 1;
+        this.adjacencyMatrix[destination][source] = 1; // for undirected graph
     }
 
     public void displayGraph() {
+        System.out.println("Adjacency List Representation:");
         for (Map.Entry<Integer, List<Integer>> entry : this.adjacencyList.entrySet()) {
             System.out.print(entry.getKey() + " -> ");
             for (int neighbor : entry.getValue()) {
                 System.out.print(neighbor + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("\nAdjacency Matrix Representation:");
+        for (int i = 0; i < adjacencyMatrix.length; i++) {
+            for (int j = 0; j < adjacencyMatrix[i].length; j++) {
+                System.out.print(adjacencyMatrix[i][j] + " ");
             }
             System.out.println();
         }
@@ -28,7 +43,8 @@ class Graph {
 
 public class MyGraph {
     public static void start() {
-        Graph graph = new Graph();
+        int numVertices = 5;
+        Graph graph = new Graph(numVertices);
 
         graph.addVertex(1);
         graph.addVertex(2);
@@ -41,7 +57,7 @@ public class MyGraph {
         graph.addEdge(2, 4);
         graph.addEdge(3, 5);
 
-        System.out.println("Graph Representation (Adjacency List) =>");
+        System.out.println("Graph Representation =>");
         graph.displayGraph();
     }
 }
