1d650eb126b7cb7f381857c3de661f5b0e86f1f1
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2024-07-01 23:08:10 +0530

    Variables in Java

---
+++ MyVariables.java
@@ -0,0 +1,25 @@
+package com.sraynitjsr.variables;
+
+public class MyVariables {
+    public static void start() {
+        int myInt = 10;
+        double myDouble = 3.14;
+        boolean myBoolean = true;
+        char myChar = 'A';
+        byte myByte = 127;
+        short myShort = 32000;
+        long myLong = 123456789L;
+        float myFloat = 2.5f;        
+        String myString = "Hello";
+
+        System.out.println("myInt: " + myInt);
+        System.out.println("myDouble: " + myDouble);
+        System.out.println("myBoolean: " + myBoolean);
+        System.out.println("myChar: " + myChar);
+        System.out.println("myByte: " + myByte);
+        System.out.println("myShort: " + myShort);
+        System.out.println("myLong: " + myLong);
+        System.out.println("myFloat: " + myFloat);
+        System.out.println("myString: " + myString);
+    }
+}
