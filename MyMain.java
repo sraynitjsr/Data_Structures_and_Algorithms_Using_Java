9ca44219489eb3b44454960234915875b0d63bcb
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2024-07-01 23:07:37 +0530

    Variables in Java

--- MyMain.java
+++ MyMain.java
@@ -1,7 +1,12 @@
 package com.sraynitjsr.main;
 
+import com.sraynitjsr.variables.*;
+
 public class MyMain {
     public static void main(String[] args) {
         System.out.println("Data Structures and Algorithms Using Java");
+        
+        System.out.println("Variables Package");
+        MyVariables.start();
     }
 }
