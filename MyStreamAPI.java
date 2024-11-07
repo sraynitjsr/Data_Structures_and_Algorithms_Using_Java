77706a5dc8883b5964ae4e152630c315d065a4a4
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2024-11-08 00:41:41 +0530

    List Using Stream API

--- MyStreamAPI.java
+++ MyStreamAPI.java
@@ -1,7 +1,21 @@
 package com.sraynitjsr.streams;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class MyStreamAPI {
     public static void start() {
-      System.out.println("Stream API Using Java");
+        System.out.println("Stream API Using Java");
+
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        numbers.stream()
+               .filter(num -> num % 2 == 0)
+               .forEach(System.out::println);
+
+        System.out.println("\n");
+
+        numbers.stream()
+               .map(num -> num * 2)
+               .forEach(System.out::println);
     }
 }
