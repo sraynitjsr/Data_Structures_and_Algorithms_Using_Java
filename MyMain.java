204042f1858d43239ca1e7f65ec24658bbab7e10
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2024-11-08 00:36:51 +0530

    Stream API Using Java

--- MyMain.java
+++ MyMain.java
@@ -2,6 +2,7 @@ package com.sraynitjsr.main;
 
 import com.sraynitjsr.variables.*;
 import com.sraynitjsr.inputout.*;
+import com.sraynitjsr.streams.*;
 
 public class MyMain {
     public static void main(String[] args) {
@@ -12,5 +13,8 @@ public class MyMain {
 
         System.out.println("Taking Input of Various Types and Output Them");
         MyInputOutput.start();
+
+        System.out.println("Stream API Using Java");
+        MyStreamAPI.start();
     }
 }
