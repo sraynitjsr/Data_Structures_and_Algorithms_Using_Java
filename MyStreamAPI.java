0cbe31afdd1d866cdb099892bb85ef5985880602
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2024-11-08 00:45:17 +0530

    Stream API Examples => Sort, Reduce, Collect, Distinct and FlatMap

--- MyStreamAPI.java
+++ MyStreamAPI.java
@@ -1,7 +1,7 @@
 package com.sraynitjsr.streams;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class MyStreamAPI {
     public static void start() {
@@ -17,5 +17,42 @@ public class MyStreamAPI {
         numbers.stream()
                .map(num -> num * 2)
                .forEach(System.out::println);
+
+        System.out.println("\n");
+
+        numbers.stream()
+               .sorted()
+               .forEach(System.out::println);
+
+        System.out.println("\n");
+
+        int sum = numbers.stream()
+                         .reduce(0, Integer::sum);
+        System.out.println("Sum of numbers: " + sum);
+
+        System.out.println("\n");
+
+        List<Integer> evenNumbers = numbers.stream()
+                                           .filter(num -> num % 2 == 0)
+                                           .collect(Collectors.toList());
+        System.out.println("Even numbers: " + evenNumbers);
+
+        System.out.println("\n");
+
+        List<Integer> duplicates = Arrays.asList(1, 2, 2, 3, 4, 4, 5);
+        duplicates.stream()
+                  .distinct()
+                  .forEach(System.out::println);
+
+        System.out.println("\n");
+
+        List<List<Integer>> listOfLists = Arrays.asList(
+                Arrays.asList(1, 2, 3),
+                Arrays.asList(4, 5),
+                Arrays.asList(6, 7)
+        );
+        listOfLists.stream()
+                   .flatMap(List::stream)
+                   .forEach(System.out::println);
     }
 }
